103bc2ce6427c2e5bf33197615e2347abc5c0a24
Jones Ogolo <jay.topher11@gmail.com>
2022-04-08 20:44:40 +0100

    add values to provider

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Box, Container } from "@mui/material";
+import Navbar from "./components/Navbar";
+import DataTable from "./components/DataTable";
+import { useEffect, useState } from "react";
+import { fetchData } from "./utils/api";
+import TableFilters from "./components/TableFilters";
+import CompanyContext from "./context/companyContext";
+import { filterFunction } from "./utils/commons";
 
 function App() {
+  const [partnerCompanies, setPartnerCompanies] = useState([]);
+  const [filteredCompanies, setFilteredCompanies] = useState([]);
+  const [selectedPaymentModes, setSelectedPaymentModes] = useState([]);
+  const [status, setStatus] = useState("");
+  const [searchText, setSearchText] = useState("");
+  const isFiltering =
+    selectedPaymentModes.length > 0 || Boolean(status) || Boolean(searchText);
+  useEffect(() => {
+    fetchData(setPartnerCompanies);
+  }, []);
+  useEffect(() => {
+    const filtered = filterFunction(partnerCompanies, {
+      searchText,
+      status,
+      paymentModes: selectedPaymentModes,
+    });
+    setFilteredCompanies(filtered);
+  }, [selectedPaymentModes, status, searchText, partnerCompanies]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CompanyContext.Provider
+      value={{
+        partnerCompanies,
+        filteredCompanies,
+        setFilteredCompanies,
+        selectedPaymentModes,
+        setSelectedPaymentModes,
+        status,
+        setStatus,
+        searchText,
+        setSearchText,
+        isFiltering,
+      }}
+    >
+      <Box>
+        <Navbar />
+        <Box component="main" sx={{ mt: 3 }}>
+          <Container maxWidth="lg">
+            <TableFilters />
+            <DataTable />
+          </Container>
+        </Box>
+      </Box>
+    </CompanyContext.Provider>
   );
 }
 
